c44707abe5c925d9970a2eb90758325bd3f4e24d
Rix565 <61545240+Rix565@users.noreply.github.com>
2021-05-11 19:17:46 +0200

    Code updated ! (yay)

--- routes.js
+++ routes.js
@@ -4,6 +4,12 @@ const router = express.Router();
 router.get('/', function (req, res) {
     res.render('index')
 })
+router.get('/activity', function (req, res) {
+    res.render('activity')
+})
+router.get('/communities/search', function(req, res){
+    res.render('community-search')
+})
 // just a test do not ask about it
 router.get('/403', function (req, res) {
     res.render('403')
